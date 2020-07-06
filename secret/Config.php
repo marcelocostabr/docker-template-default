0f87f88af86cae95e19d344d2e5c5c23df4fc9ad
Marcelo Costa <marcelo@mcosta.dev>
2020-07-06 09:36:28 -0300

    fix database connection between docker containers

---
+++ secret/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Database credentials
+
+# important the host must have
+# the same name of the container_name
+# in docker-compose.yml
+$host = 'mariadb-server';
+$user = 'root';
+$pass = 'root';
+$base = 'db_name';
